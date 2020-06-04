03367e35315d491934891308f9a480393a048560
Danny Befumo <DPBefumo@gmail.com>
2020-06-04 19:09:31 -0400

    quality check of templates

--- snippets/views.py
+++ snippets/views.py
@@ -25,7 +25,7 @@ def show_snippet(request, snippet_pk):
 
 
 @login_required
-def add_snippet(request): #form
+def add_snippet(request):
     if request.method == "POST":
         form = SnippetForm(data=request.POST)
         if form.is_valid():
@@ -42,10 +42,20 @@ def add_snippet(request): #form
 @login_required
 def edit_snippet(request, snippet_pk):
     snippet = get_object_or_404(request.user.snippets, pk=snippet_pk)
-    return render(request, "snippets/edit_snippet.html", {'snippet':snippet})
+    
+    if request.method == "POST":
+        form = SnippetForm(instance=snippet, data=request.POST)
+        if form.is_valid():
+            snippet = form.save()
+            return redirect(to='show_snippet', snippet_pk=snippet.pk)
+
+    else:
+        form = SnippetForm()
+    
+    return render(request, "snippets/edit_snippet.html", {'snippet': snippet, 'form': form})
 
 
-@login_required
+@login_required #neds to be edited
 def delete_snippet(request, snippet_pk):
     snippet = get_object_or_404(request.user.snippets, pk=snippet_pk)
-    return render(request, "snippets/delete_snippet.html", {'snippet':snippet})
+    return render(request, "snippets/delete_snippet.html", {'snippet': snippet})
