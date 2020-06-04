3790248a3d83b6c0d1ab180beec77bce96e565be
Danny Befumo <DPBefumo@gmail.com>
2020-06-04 09:31:20 -0400

    added a few pages, cannoy make migrations

--- snippets/models.py
+++ snippets/models.py
@@ -2,4 +2,11 @@ from django.db import models
 from users.models import User
 
 class Snippet(models.Model):
-    pass
+    # user = models.ForeignKey(to=User,
+    #                         on_delete=models.CASCADE,
+    #                         related_name="snippets")
+    title = models.CharField(max_length=255)
+    time_stamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Snippet
+from users.models import User
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 def home_page(request):
-    snippets = Snippet.objects.all()
-    return render(request, "snippets/home_page.html", {'snippets': snippets})
+    return render(request, "snippets/home_page.html")
+
+@login_required
+def list_snippet(request):
+    snippets = request.user.snippets.all()
+    return render(request, "snippets/list_snippet.html", {'snippets': snippets})
+
+@login_required
+def show_snippet(request, snippet_pk):
+    snippet = get_object_or_404(request.user.snippets, pk=snippet_pk)
+    return render(request, "snippets/show_snippet.html", {"snippet": snippet})
