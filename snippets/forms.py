e34b35b3d082352026878c66f9dbadad25910d4d
Danny Befumo <DPBefumo@gmail.com>
2020-06-09 22:55:52 -0400

    finished styling for the day

--- snippets/forms.py
+++ snippets/forms.py
@@ -19,5 +19,5 @@ class SnippetForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'pa2 f4 w-100 bg-silver'}),
             'language': forms.TextInput(attrs={'class': 'pa2 f4 w-100 bg-silver'}),
             'description': forms.TextInput(attrs={'class': 'pa2 f4 w-100 bg-silver'}),
-            'body': forms.Textarea(attrs={'class': 'pa2 f4 h4 w-100 bg-silver'}),
+            'body': forms.Textarea(attrs={'class': 'pa2 f4 h4 w-100 h5 bg-silver'}),
         } 
